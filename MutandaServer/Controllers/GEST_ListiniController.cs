1789b0e287079668a79311a85dea2b72d272c5b4
Fabrizio Di Donna <f.didonna@hotmail.com>
2016-06-12 11:37:46 +0200

    Aggiunta tag listini per prova merge / branch

--- MutandaServer/Controllers/GEST_ListiniController.cs
+++ MutandaServer/Controllers/GEST_ListiniController.cs
@@ -5,9 +5,11 @@ using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using OrderEntry.Net.Models;
+using Microsoft.Azure.Mobile.Server.Config;
 
 namespace OrderEntry.Net.Service
 {
+    [MobileAppController]
     public class GEST_ListiniController : BaseController<GEST_Listini>
     {
         protected OrderEntryNetContext context;
