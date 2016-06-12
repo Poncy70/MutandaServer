4b9ea440e821c9820816b4e9df8520f4e737375e
Fabrizio Di Donna <f.didonna@hotmail.com>
2016-06-12 11:15:59 +0200

    Gestione Api controller personalizzato

--- MutandaServer/Controllers/OrdiniSaveApiController.cs
+++ MutandaServer/Controllers/OrdiniSaveApiController.cs
@@ -127,127 +127,126 @@ namespace OrderEntry.Net.Service
 
         private PushResult Insert(GEST_Ordini_Teste ordine)
         {
-            return null;
-            //PushResult result = null;
+            PushResult result = null;
 
-            //StringBuilder sqlTesta = new StringBuilder();
-            //StringBuilder sqlRiga = new StringBuilder();
-            //List<string> sqlRighe = new List<string>();
+            StringBuilder sqlTesta = new StringBuilder();
+            StringBuilder sqlRiga = new StringBuilder();
+            List<string> sqlRighe = new List<string>();
 
-            //ConnectionInfo connectionInfo = ControllerStatic.GetDBSource(mCredentials);
-            //DBData db = new DBData(connectionInfo);
+            ConnectionInfo connectionInfo = ControllerStatic.GetDBSource(mCredentials);
+            DBData db = new DBData(connectionInfo);
 
-            //try
-            //{
-            //    string numeroOrdineDevice = GetNumeroOrdine(ordine.DeviceMail);
+            try
+            {
+                string numeroOrdineDevice = GetNumeroOrdine(ordine.DeviceMail);
 
-            //    if (numeroOrdineDevice != string.Empty)
-            //    {
-            //        sqlTesta.Append("INSERT INTO GEST_Ordini_Teste(Id, RagioneSociale, PartitaIva, CodiceFiscale, DataDocumento, NumeroOrdineDevice, ");
-            //        sqlTesta.Append("CodPagamento, CodListino, TotaleDocumento, CloudState, DataConsegna, TotaleConsegna, IdAgente, IdAnagrafica, Note, ");
-            //        sqlTesta.Append("IdIndSpedMerce, RagSocSped, IndirizzoSped, CittaSped, CapSped, ProvSped, NazioneSped, NrRigheTot, DeviceMail, IdDevice, UpdatedAt) ");
-            //        sqlTesta.Append("VALUES(");
-            //        sqlTesta.AppendFormat("'{0}',", ordine.Id);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.RagioneSociale);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.PartitaIva);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.CodiceFiscale);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.DataDocumento.Date.ToString("yyyy-MM-dd HH:mm:ss"));
-            //        sqlTesta.AppendFormat("'{0}',", numeroOrdineDevice);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.CodPagamento);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.CodListino);
-            //        sqlTesta.AppendFormat("{0},", ordine.TotaleDocumento);
-            //        sqlTesta.AppendFormat("{0},", ordine.CloudState);
+                if (numeroOrdineDevice != string.Empty)
+                {
+                    sqlTesta.Append("INSERT INTO GEST_Ordini_Teste(Id, RagioneSociale, PartitaIva, CodiceFiscale, DataDocumento, NumeroOrdineDevice, ");
+                    sqlTesta.Append("CodPagamento, CodListino, TotaleDocumento, CloudState, DataConsegna, TotaleConsegna, IdAgente, IdAnagrafica, Note, ");
+                    sqlTesta.Append("IdIndSpedMerce, RagSocSped, IndirizzoSped, CittaSped, CapSped, ProvSped, NazioneSped, NrRigheTot, DeviceMail, IdDevice, UpdatedAt) ");
+                    sqlTesta.Append("VALUES(");
+                    sqlTesta.AppendFormat("'{0}',", ordine.Id);
+                    sqlTesta.AppendFormat("'{0}',", ordine.RagioneSociale);
+                    sqlTesta.AppendFormat("'{0}',", ordine.PartitaIva);
+                    sqlTesta.AppendFormat("'{0}',", ordine.CodiceFiscale);
+                    sqlTesta.AppendFormat("'{0}',", ordine.DataDocumento.Date.ToString("yyyy-MM-dd HH:mm:ss"));
+                    sqlTesta.AppendFormat("'{0}',", numeroOrdineDevice);
+                    sqlTesta.AppendFormat("'{0}',", ordine.CodPagamento);
+                    sqlTesta.AppendFormat("'{0}',", ordine.CodListino);
+                    sqlTesta.AppendFormat("{0},", ordine.TotaleDocumento);
+                    sqlTesta.AppendFormat("{0},", ordine.CloudState);
 
-            //        if (ordine.DataConsegna.HasValue)
-            //            sqlTesta.AppendFormat("'{0}',", ordine.DataConsegna.Value.Date.ToString("yyyy-MM-dd HH:mm:ss"));
-            //        else
-            //            sqlTesta.AppendFormat("{0},", "Null");
+                    if (ordine.DataConsegna.HasValue)
+                        sqlTesta.AppendFormat("'{0}',", ordine.DataConsegna.Value.Date.ToString("yyyy-MM-dd HH:mm:ss"));
+                    else
+                        sqlTesta.AppendFormat("{0},", "Null");
 
-            //        sqlTesta.AppendFormat("{0},", ordine.TotaleConsegna);
-            //        sqlTesta.AppendFormat("{0},", ordine.IdAgente);
-            //        sqlTesta.AppendFormat("{0},", ordine.IdAnagrafica);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.Note);
-            //        sqlTesta.AppendFormat("{0},", ordine.IdIndSpedMerce);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.RagSocSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.IndirizzoSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.CittaSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.CapSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.ProvSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.NazioneSped);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.NrRigheTot);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.DeviceMail);
-            //        sqlTesta.AppendFormat("'{0}',", ordine.IdDevice);
-            //        sqlTesta.Append("getDate()");
-            //        sqlTesta.Append(")");
+                    sqlTesta.AppendFormat("{0},", ordine.TotaleConsegna);
+                    sqlTesta.AppendFormat("{0},", ordine.IdAgente);
+                    sqlTesta.AppendFormat("{0},", ordine.IdAnagrafica);
+                    sqlTesta.AppendFormat("'{0}',", ordine.Note);
+                    sqlTesta.AppendFormat("{0},", ordine.IdIndSpedMerce);
+                    sqlTesta.AppendFormat("'{0}',", ordine.RagSocSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.IndirizzoSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.CittaSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.CapSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.ProvSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.NazioneSped);
+                    sqlTesta.AppendFormat("'{0}',", ordine.NrRigheTot);
+                    sqlTesta.AppendFormat("'{0}',", ordine.DeviceMail);
+                    sqlTesta.AppendFormat("'{0}',", ordine.IdDevice);
+                    sqlTesta.Append("getDate()");
+                    sqlTesta.Append(")");
 
-            //        if (ordine.RigheOrdine.Count > 0)
-            //        {
-            //            string guiRiga;
+                    if (ordine.RigheOrdine.Count > 0)
+                    {
+                        string guiRiga;
 
-            //            foreach (GEST_Ordini_RigheDTO riga in ordine.RigheOrdine)
-            //            {
-            //                guiRiga = Guid.NewGuid().ToString();
+                        foreach (GEST_Ordini_Righe riga in ordine.RigheOrdine)
+                        {
+                            guiRiga = Guid.NewGuid().ToString();
 
-            //                sqlRiga.Clear();
-            //                sqlRiga.Append("INSERT INTO GEST_Ordini_Righe(Id, IdSlave, IdRiga, TipoRiga, CodArt, Descrizione, Qta, NCP_QtaScontoMerce, ");
-            //                sqlRiga.Append("CodUnMis, CodIva, ValUnit, Sc1, Sc2, Sc3, Sc4, ImportoSconto, DataPresuntaConsegna, Imponibile, Imposta, Totale, UpdatedAt) ");
-            //                sqlRiga.Append("VALUES(");
-            //                sqlRiga.AppendFormat("'{0}',", riga.Id);
-            //                sqlRiga.AppendFormat("'{0}',", ordine.Id);
-            //                sqlRiga.AppendFormat("'{0}',", riga.IdRiga);
-            //                sqlRiga.AppendFormat("{0},", riga.TipoRiga);
-            //                sqlRiga.AppendFormat("'{0}',", riga.CodArt);
-            //                sqlRiga.AppendFormat("'{0}',", riga.Descrizione);
-            //                sqlRiga.AppendFormat("{0},", riga.Qta);
-            //                sqlRiga.AppendFormat("{0},", riga.NCP_QtaScontoMerce);
-            //                sqlRiga.AppendFormat("'{0}',", riga.CodUnMis);
-            //                sqlRiga.AppendFormat("'{0}',", riga.CodIva);
-            //                sqlRiga.AppendFormat("{0},", riga.ValUnit);
-            //                sqlRiga.AppendFormat("{0},", riga.Sc1);
-            //                sqlRiga.AppendFormat("{0},", riga.Sc2);
-            //                sqlRiga.AppendFormat("{0},", riga.Sc3);
-            //                sqlRiga.AppendFormat("{0},", riga.Sc4);
-            //                sqlRiga.AppendFormat("{0},", riga.ImportoSconto);
+                            sqlRiga.Clear();
+                            sqlRiga.Append("INSERT INTO GEST_Ordini_Righe(Id, IdSlave, IdRiga, TipoRiga, CodArt, Descrizione, Qta, NCP_QtaScontoMerce, ");
+                            sqlRiga.Append("CodUnMis, CodIva, ValUnit, Sc1, Sc2, Sc3, Sc4, ImportoSconto, DataPresuntaConsegna, Imponibile, Imposta, Totale, UpdatedAt) ");
+                            sqlRiga.Append("VALUES(");
+                            sqlRiga.AppendFormat("'{0}',", riga.Id);
+                            sqlRiga.AppendFormat("'{0}',", ordine.Id);
+                            sqlRiga.AppendFormat("'{0}',", riga.IdRiga);
+                            sqlRiga.AppendFormat("{0},", riga.TipoRiga);
+                            sqlRiga.AppendFormat("'{0}',", riga.CodArt);
+                            sqlRiga.AppendFormat("'{0}',", riga.Descrizione);
+                            sqlRiga.AppendFormat("{0},", riga.Qta);
+                            sqlRiga.AppendFormat("{0},", riga.NCP_QtaScontoMerce);
+                            sqlRiga.AppendFormat("'{0}',", riga.CodUnMis);
+                            sqlRiga.AppendFormat("'{0}',", riga.CodIva);
+                            sqlRiga.AppendFormat("{0},", riga.ValUnit);
+                            sqlRiga.AppendFormat("{0},", riga.Sc1);
+                            sqlRiga.AppendFormat("{0},", riga.Sc2);
+                            sqlRiga.AppendFormat("{0},", riga.Sc3);
+                            sqlRiga.AppendFormat("{0},", riga.Sc4);
+                            sqlRiga.AppendFormat("{0},", riga.ImportoSconto);
 
-            //                if (riga.DataPresuntaConsegna.HasValue)
-            //                    sqlRiga.AppendFormat("'{0}',", riga.DataPresuntaConsegna.Value.Date.ToString("yyyy-MM-dd HH:mm:ss"));
-            //                else
-            //                    sqlRiga.AppendFormat("{0},", "Null");
+                            if (riga.DataPresuntaConsegna.HasValue)
+                                sqlRiga.AppendFormat("'{0}',", riga.DataPresuntaConsegna.Value.Date.ToString("yyyy-MM-dd HH:mm:ss"));
+                            else
+                                sqlRiga.AppendFormat("{0},", "Null");
 
-            //                sqlRiga.AppendFormat("{0},", riga.Imponibile);
-            //                sqlRiga.AppendFormat("{0},", riga.Imposta);
-            //                sqlRiga.AppendFormat("{0},", riga.Totale);
-            //                sqlRiga.Append("getDate()");
-            //                sqlRiga.Append(")");
+                            sqlRiga.AppendFormat("{0},", riga.Imponibile);
+                            sqlRiga.AppendFormat("{0},", riga.Imposta);
+                            sqlRiga.AppendFormat("{0},", riga.Totale);
+                            sqlRiga.Append("getDate()");
+                            sqlRiga.Append(")");
 
-            //                sqlRighe.Add(sqlRiga.ToString());
-            //            }
-            //        }
+                            sqlRighe.Add(sqlRiga.ToString());
+                        }
+                    }
 
-            //        db.BeginTransaction();
-            //        db.Execute(sqlTesta.ToString());
+                    db.BeginTransaction();
+                    db.Execute(sqlTesta.ToString());
 
-            //        foreach (string riga in sqlRighe)
-            //            db.Execute(riga);
+                    foreach (string riga in sqlRighe)
+                        db.Execute(riga);
 
-            //        db.CommitTransaction();
+                    db.CommitTransaction();
 
-            //        result = new PushResult() { Message = "Inserimento Effettuato.", NumeroOrdineDevice = numeroOrdineDevice, OK = true };
-            //    }
-            //    else
-            //        result = new PushResult() { Message = "Inserimento fallito. Impossibile attribuire un numero d'ordine", NumeroOrdineDevice = "", OK = false };
-            //}
-            //catch (Exception ex)
-            //{
-            //    if (db.TransactionOn)
-            //        db.RollbackTransaction();
+                    result = new PushResult() { Message = "Inserimento Effettuato.", NumeroOrdineDevice = numeroOrdineDevice, OK = true };
+                }
+                else
+                    result = new PushResult() { Message = "Inserimento fallito. Impossibile attribuire un numero d'ordine", NumeroOrdineDevice = "", OK = false };
+            }
+            catch (Exception ex)
+            {
+                if (db.TransactionOn)
+                    db.RollbackTransaction();
 
-            //    ControllerStatic.WriteErrorLog(connectionInfo, "OrdiniSaveController", ex, "TESTA: " + sqlTesta.ToString() + " RIGHE: " + sqlRiga.ToString());
+                ControllerStatic.WriteErrorLog(connectionInfo, "OrdiniSaveController", ex, "TESTA: " + sqlTesta.ToString() + " RIGHE: " + sqlRiga.ToString());
 
-            //    result = new PushResult() { Message = "Inserimento fallito.", NumeroOrdineDevice = "", OK = false };
-            //}
+                result = new PushResult() { Message = "Inserimento fallito.", NumeroOrdineDevice = "", OK = false };
+            }
 
-            //return result;
+            return result;
         }
 
         private async Task RunSafe(Task task, bool notifyOnError = true, [CallerMemberName] string caller = "")
