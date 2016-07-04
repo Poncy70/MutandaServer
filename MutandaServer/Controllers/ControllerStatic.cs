7f8a02ab9f9f23b8c6933e0e0c29d36a81cc6530
Fabrizio Di Donna <f.didonna@hotmail.com>
2016-07-04 10:31:56 +0200

    Aggiornamento versione

--- MutandaServer/Controllers/ControllerStatic.cs
+++ MutandaServer/Controllers/ControllerStatic.cs
@@ -15,43 +15,31 @@ namespace OrderEntry.Net.Service
             ///
             try
             {
-            StringBuilder sql = new StringBuilder();
-            sql.Append("SELECT a.servername, a.dbname, a.dbuser, a.dbpassword, a.devicemail, IsNull(a.idagente, 0) as idagente, IsNull(a.superuser, 0) as superuser ");
-            sql.Append("FROM [orderEntry].[Autenticate] a ");
-            sql.AppendFormat("WHERE a.devicemail = '{0}' ", credentials.UserId);
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT a.servername, a.dbname, a.dbuser, a.dbpassword, a.devicemail, IsNull(a.idagente, 0) as idagente, IsNull(a.superuser, 0) as superuser ");
+                sql.Append("FROM [orderEntry].[Autenticate] a ");
+                sql.AppendFormat("WHERE a.devicemail = '{0}' ", credentials.UserId);
 
-            DataTable dt = null;
-            string deviceMail = "";
-            int idAgente = 0;
-
-            try
-            {
+                DataTable dt = null;
                 dt = db.ReadData(sql.ToString());
 
-                DBData dbAgente = new DBData("mxeqxlr2h5.database.windows.net,1433", "LamandeDB", "TrilogikSa", "Password.1");
-                string sqlAgente = "SELECT DeviceMail, IdAgente FROM DEVICE_ParametriDevice WHERE DeviceMail = '" + credentials.UserId + "'";
-                DataTable dtAgente = dbAgente.ReadData(sqlAgente);
-
-                if (dtAgente.Rows.Count > 0)
+                if (dt.Rows.Count > 0)
                 {
-                    deviceMail = (string)dtAgente.Rows[0]["DeviceMail"];
-                    idAgente = (int)dtAgente.Rows[0]["IdAgente"];
+                    connectionInfo.ServerName = (string)dt.Rows[0]["servername"];
+                    connectionInfo.DBName = (string)dt.Rows[0]["dbname"];
+                    connectionInfo.DBUser = (string)dt.Rows[0]["dbuser"];
+                    connectionInfo.DBPassword = (string)dt.Rows[0]["dbpassword"];
+                    connectionInfo.SuperUser = (bool)dt.Rows[0]["superuser"];
+                    connectionInfo.DeviceMail = (string)dt.Rows[0]["devicemail"];
+                    connectionInfo.IdAgente = (int)dt.Rows[0]["idagente"];
                 }
+
+                if (connectionInfo.IdAgente == 0)
+                    WriteErrorLog(connectionInfo, "ControllerStatic.GetDBSource", credentials.UserId);
             }
             catch (System.Exception ex)
             {
-                throw ex;
-            }
-            
-            if (dt.Rows.Count > 0)
-            {
-                connectionInfo.ServerName = (string)dt.Rows[0]["servername"];
-                connectionInfo.DBName = (string)dt.Rows[0]["dbname"];
-                connectionInfo.DBUser = (string)dt.Rows[0]["dbuser"];
-                connectionInfo.DBPassword = (string)dt.Rows[0]["dbpassword"];
-                connectionInfo.DeviceMail = deviceMail;
-                connectionInfo.IdAgente = idAgente;
-                connectionInfo.SuperUser = (bool)dt.Rows[0]["superuser"];
+                WriteErrorLog(connectionInfo, "ControllerStatic.GetDBSource", ex.Message);
             }
 
             return connectionInfo;
@@ -63,9 +51,9 @@ namespace OrderEntry.Net.Service
             StringBuilder sql = new StringBuilder();
 
             sql.Append("INSERT INTO ErrorLog(ErrorDate, DeviceMail, Controller, MessageText, InnerException, StackTrace, SqlString) ");
-            sql.AppendFormat("VALUES(getdate(), '{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", connectionInfo.DeviceMail, controller, 
-                              ex.Message.Replace("'", "''"), ex.InnerException != null ? 
-                              ex.InnerException.ToString().Replace("'", "''"): "", ex.StackTrace.Replace("'", "''"), sqlString);
+            sql.AppendFormat("VALUES(getdate(), '{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", connectionInfo.DeviceMail, controller,
+                              ex.Message.Replace("'", "''"), ex.InnerException != null ?
+                              ex.InnerException.ToString().Replace("'", "''") : "", ex.StackTrace.Replace("'", "''"), sqlString);
 
             db.Execute(sql.ToString());
             db.CloseConnection();
